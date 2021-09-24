4b08e8728e88cce431450f367e10b3a2950a4bc8
joseRdzA <jose.rodriguez.arias.ucreativa@gmail.com>
2021-09-23 20:48:29 -0600

    Trabajo Semana 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,77 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) { //void no retorna nada
-	System.out.println("hola mundo");
+    static String metodo(String variable) {
+        //static para poder referenciarlo en el main
+        variable = variable + "Mundo";
+        return variable;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hola clase");
     /*
     Variables
     Type nombre = valor;*/
 
-
-    int entero = 2;
-    float flotante = 2.5f;
-    String cadenaCaracteres = "Entre doble comilla";
-    System.out.println(cadenaCaracteres);
+        String variable = "hola";
+        variable = metodo(variable);
 
 
+        int entero = 2;
+        float flotante = 2.5f;
+        String cadenaCaracteres = "2";
+        Integer entero2 = 2;
+        Boolean siOno = true;
+
+        System.out.println(cadenaCaracteres);
+        System.out.println("------------------------------------");
+        System.out.println(Integer.parseInt(cadenaCaracteres) + entero);
+        System.out.println("------------------------------------");
+        System.out.println(Integer.parseInt(cadenaCaracteres));
+        System.out.println("------------------------------------");
+//Int a integer, uno es primitivo y el otro orientación
+//Condicionales
+        if (entero == 2) {
+            System.out.println("Es el numero correcto");
+        } else {
+            System.out.println("No es el numero correcto");
+        }
+
+        if (entero == 2) {
+            System.out.println("Es el numero correcto, else if");
+        } else if (entero == 3) {
+            System.out.println("No es el numero correcto");
+        } else {
+            System.out.println("Que fue lo que puso?");
+        }
+
+        switch (entero) {
+            case 1:
+                System.out.println("Si es uno");
+                System.out.println("Que si es uno");
+                break;
+            default:
+                System.out.println("Default");
+        }
+
+        //Loops
+       /* int i = 0;
+        while (1 < 10) {
+            System.out.println("Por aquí voy: " + i);
+            i = i + 1;
+        }*/
+
+        /*for(int j = 0; j<10; j++){
+            System.out.println("Por aquí va el otro" + j);
+        }*/
+
+        //Arrays
+
+        String[] objetos = {"Un objeto", "Dos objetos"};
+        System.out.println("Que tiene el 0: " + objetos[0]);
 
 
     }
 }
+//format control + alt + l
