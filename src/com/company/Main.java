17c7dfde9c7ec5c0b68d5da091f079a7e8db527b
joseRdzA <jose.rodriguez.arias.ucreativa@gmail.com>
2021-09-23 20:07:39 -0600

    Actualizo el gitIgnore y agregué el Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //void no retorna nada
 	System.out.println("hola mundo");
+    /*
+    Variables
+    Type nombre = valor;*/
+
+
+    int entero = 2;
+    float flotante = 2.5f;
+    String cadenaCaracteres = "Entre doble comilla";
+    System.out.println(cadenaCaracteres);
+
+
+
 
     }
 }
