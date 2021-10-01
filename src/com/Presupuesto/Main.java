da9a854507df89a53a0a5c3ec255e0f2a9073d03
joseRdzA <jose.rodriguez.arias.ucreativa@gmail.com>
2021-09-30 20:46:16 -0600

    Trabajo en clase 30-9-2021 #4

--- src/com/Presupuesto/Main.java
+++ src/com/Presupuesto/Main.java
@@ -1,44 +1,66 @@
 package com.Presupuesto;
 
+import com.Presupuesto.entidades.Gasto;
+import com.Presupuesto.entidades.RegistroGastos;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     //Escribir esto para correr el sistema
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("Nuevo Main");
-        System.out.println("-------------------------");
-
-        //Instanciar un objeto para poder usar la clase. El objeto es una clase instanciada y agarra lo que tengo en la clase y le asigna memoria.
-
-        Gasto gastoLuz = new Gasto();
-        gastoLuz.nombre = "Luz";
-        gastoLuz.moneda = "Colones";
-        gastoLuz.monto = 20000;
-        System.out.println(gastoLuz.nombre + " -> " + gastoLuz.monto);
-
-
-        Gasto gastoInternet = new Gasto();
-        gastoInternet.nombre = "Internet";
-        gastoLuz.moneda = "Colones";
-        gastoInternet.monto = 15000;
-        System.out.println(gastoInternet.nombre + " -> " + gastoInternet.monto);
-
-
+        System.out.println("Sistema Registro de Gastos");
+        Scanner consola = new Scanner(System.in);
+        boolean siga = true;
         RegistroGastos registro = new RegistroGastos();
-        registro.gastos = new ArrayList<>();
-        registro.gastos.add(gastoLuz);
-        registro.gastos.add(gastoInternet);
+        while (siga) {
 
-        System.out.println(gastoInternet.nombre + " -> " + gastoInternet.monto);
-        System.out.println(gastoLuz.nombre + " -> " + gastoLuz.monto);
+            System.out.println("Digite el nombre de su gasto:");
+            String nombre = consola.nextLine();
 
-        System.out.println("---------------------------------------------------------------");
+            System.out.println("Digite la moneda de su gasto:");
+            String moneda = consola.nextLine();
 
-        for (int i = 0; i<registro.gastos.size(); i++){
-            System.out.println(registro.gastos.get(i).nombre);
+            System.out.println("Digite la categoría de su gasto:");
+            String categoria = consola.nextLine();
+
+            System.out.println("Digite el monto de su gasto:");
+            String montoStr = consola.nextLine();
+            int monto = Integer.parseInt(montoStr);
+
+
+            //Instanciar un objeto para poder usar la clase. El objeto es una clase instanciada y agarra lo que tengo en la clase y le asigna memoria.
+
+            Gasto nuevoGasto = new Gasto(nombre,
+                    moneda, categoria, monto);
+
+            registro.addGastos(nuevoGasto);
+
+            ;
+
+
+       /* for (int i = 0; i<registro.getGastos().size(); i++){
+            System.out.println(registro.getGastos().get(i).getNombre());
+        }*/
+
+        /*for (Gasto gastico :registro.getGastos()){
+            //declaro primero la variable
+            System.out.println(gastico.getNombre());
+        }*/
+
+        /*for(Gasto gastico : registro.getGastos()){
+        * System.out.pprintln(gastico.getNombre());
+        }*/
+
+            for (Gasto gastico : registro.getGastos()) {
+                System.out.println(gastico.getNombre());
+            }
+
+            System.out.println("Quiere seguir? ('S')");
+            consola.nextLine();
+            siga = consola.nextLine().equals("S");
+
         }
-
 
 
     }
--- src/com/Presupuesto/RegistroGastos.java
+++
@@ -1,9 +0,0 @@
-package com.Presupuesto;
-
-import java.util.List;
-
-public class RegistroGastos {
-
-   List<Gasto> gastos;
-
-}
